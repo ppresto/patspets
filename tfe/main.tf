164a25ac9ee3521e190eb9dc65f304871a5cf179
ppresto <ppresto@hashicorp.com>
2019-09-12 15:10:25 -0700

    add instance

--- tfe/main.tf
+++ tfe/main.tf
@@ -1,26 +1,12 @@
 //--------------------------------------------------------------------
-// Module - GCP Instances
-module "gce_instance" {
-  source  = "app.terraform.io/Patrick/gce_instance/google"
-  version = "0.1.4"
-  name_prefix = "${replace(var.name_prefix,"/_/","-")}"  #replace _ to meet GCE naming requirements
-  count = 0
-  machine_type = "n1-standard-2"
-}
-
-output "GCP_Address" {
-  value = "${module.gce_instance.addresses}"
-}
-
-//--------------------------------------------------------------------
 // Module - AWS Instances
 module "ec2_instance" {
   source  = "app.terraform.io/Patrick/ec2_instance/aws"
-  version = "0.1.5"
+  version = "0.1.6"
   name_prefix = "${var.name_prefix}"
-  count = 0
+  count = 1
   instance_type = "t2.micro"
-  subnet_id = "${data.terraform_remote_state.patrick_tf_aws_standard_network.subnet_private_ids[0]}"
+  securitygroup_id = "${data.terraform_remote_state.patrick_tf_aws_standard_network.webapp_security_group}"
 }
 
 //--------------------------------------------------------------------
