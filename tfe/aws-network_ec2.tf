9b3f27583e39a0b286bd0c84478a8d1ca95a0a3e
ppresto <ppresto@hashicorp.com>
2020-01-22 10:42:31 -0800

    test1

--- tfe/aws-network_ec2.tf
+++ tfe/aws-network_ec2.tf
@@ -1,9 +1,28 @@
 //--------------------------------------------------------------------
 // Modules
-module "aws_std_network" {
-  name = "${var.name_prefix}"
-  source  = "app.terraform.io/Patrick/aws_std_network/aws"
-  version = "0.2.4"
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "vpc" {
+  source  = "app.terraform.io/presto-workshop-tfc-aws/vpc/aws"
+  version = "2.21.0"
+
+  name = "presto-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "presto-dev"
+  }
 }
 
 module "ec2_instance" {
@@ -13,7 +32,7 @@ module "ec2_instance" {
   name_prefix = "${var.name_prefix}"
   instance_count = 5
   instance_type = "t2.nano"
-  security_group = "${module.aws_std_network.security_group_web}"
+  security_group = "${module.vpc.default_security_group_id}"
 }
 
 //--------------------------------------------------------------------
