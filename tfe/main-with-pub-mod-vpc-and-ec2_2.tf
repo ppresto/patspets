050cea6bb5106c9af263cf81d47f61b04b0705dd
ppresto <ppresto@hashicorp.com>
2020-01-23 12:20:50 -0800

    test custom sg

--- tfe/main-with-pub-mod-vpc-and-ec2_2.tf
+++ tfe/main-with-pub-mod-vpc-and-ec2_2.tf
@@ -2,15 +2,28 @@
 //--------------------------------------------------------------------
 // Modules
 
-
 module "myapp_sg" {
-  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+  source = "terraform-aws-modules/security-group/aws"
 
-  name        = "web-server"
-  description = "Security group for web-server with HTTP ports open within VPC"
+  name        = "myapp-service"
+  description = "Security group for myapp-service with my custom ports open within VPC, and PostgreSQL publicly open"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "myapp-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
 }
 
 module "vpc" {
