410fa8fc4343103c53203b1b471b8253dd047051
ppresto <ppresto@hashicorp.com>
2020-01-23 11:39:49 -0800

    test1

--- tfe/main-with-pub-mod-vpc-and-ec2_2.tf
+++ tfe/main-with-pub-mod-vpc-and-ec2_2.tf
@@ -1,156 +1,49 @@
-provider "aws" {
-  region = "eu-west-1"
+
+//--------------------------------------------------------------------
+// Modules
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = module.vpc.vpc_id
 }
 
-##################################################################
-# Data sources to get VPC, subnet, security group and AMI details
-##################################################################
-data "aws_vpc" "default" {
-  default = true
-}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.17.0"
+  //source  = "app.terraform.io/presto-workshop-tfc-aws/vpc/aws"
+  //version = "2.21.0"
 
-data "aws_subnet_ids" "all" {
-  vpc_id = "${data.aws_vpc.default.id}"
-}
+  name = "presto-vpc"
+  cidr = "10.0.0.0/16"
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  owners = ["amazon"]
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
-  filter {
-    name = "name"
-
-    values = [
-      "amzn-ami-hvm-*-x86_64-gp2",
-    ]
-  }
-
-  filter {
-    name = "owner-alias"
-
-    values = [
-      "amazon",
-    ]
+  tags = {
+    Terraform = "true"
+    Environment = "presto-dev"
   }
 }
 
-module "security_group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 2.0"
+module "ec2_cluster" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 2.0"
 
-  name        = "example"
-  description = "Security group for example usage with EC2 instance"
-  vpc_id      = "${data.aws_vpc.default.id}"
+  name                   = "my-cluster"
+  instance_count         = 3
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "all-icmp"]
-  egress_rules        = ["all-all"]
-}
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
 
-resource "aws_eip" "this" {
-  vpc      = true
-  instance = "${module.ec2.id[0]}"
-}
-
-resource "aws_placement_group" "web" {
-  name     = "hunky-dory-pg"
-  strategy = "cluster"
-}
-
-module "ec2" {
-  source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance"
-
-  instance_count = 2
-
-  name                        = "example-normal"
-  ami                         = "${data.aws_ami.amazon_linux.id}"
-  instance_type               = "m4.large"
-  subnet_id                   = "${element(data.aws_subnet_ids.all.ids, 0)}"
-  vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
-  associate_public_ip_address = true
-  placement_group             = "${aws_placement_group.web.id}"
-
-  root_block_device = [{
-    volume_type = "gp2"
-    volume_size = 10
-  }]
-}
-
-module "ec2_with_t2_unlimited" {
-  source = "../../"
-
-  instance_count = 1
-
-  name                        = "example-t2-unlimited"
-  ami                         = "${data.aws_ami.amazon_linux.id}"
-  instance_type               = "t2.micro"
-  cpu_credits                 = "unlimited"
-  subnet_id                   = "${element(data.aws_subnet_ids.all.ids, 0)}"
-  vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
-  associate_public_ip_address = true
-}
-
-module "ec2_with_t3_unlimited" {
-  source = "../../"
-
-  instance_count = 1
-
-  name                        = "example-t3-unlimited"
-  ami                         = "${data.aws_ami.amazon_linux.id}"
-  instance_type               = "t3.large"
-  cpu_credits                 = "unlimited"
-  subnet_id                   = "${element(data.aws_subnet_ids.all.ids, 0)}"
-  vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
-  associate_public_ip_address = true
-}
-
-output "ids" {
-  description = "List of IDs of instances"
-  value       = "${module.ec2.id}"
-}
-
-output "ids_t2" {
-  description = "List of IDs of t2-type instances"
-  value       = "${module.ec2_with_t2_unlimited.id}"
-}
-
-output "public_dns" {
-  description = "List of public DNS names assigned to the instances"
-  value       = "${module.ec2.public_dns}"
-}
-
-output "vpc_security_group_ids" {
-  description = "List of VPC security group ids assigned to the instances"
-  value       = "${module.ec2.vpc_security_group_ids}"
-}
-
-output "tags" {
-  description = "List of tags"
-  value       = "${module.ec2.tags}"
-}
-
-output "placement_group" {
-  description = "List of placement group"
-  value       = "${module.ec2.placement_group}"
-}
-
-output "instance_id" {
-  description = "EC2 instance ID"
-  value       = "${module.ec2.id[0]}"
-}
-
-output "instance_public_dns" {
-  description = "Public DNS name assigned to the EC2 instance"
-  value       = "${module.ec2.public_dns[0]}"
-}
-
-output "credit_specification" {
-  description = "Credit specification of EC2 instance (empty list for not t2 instance types)"
-  value       = "${module.ec2.credit_specification}"
-}
-
-output "credit_specification_t2_unlimited" {
-  description = "Credit specification of t2-type EC2 instance"
-  value       = "${module.ec2_with_t2_unlimited.credit_specification}"
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
