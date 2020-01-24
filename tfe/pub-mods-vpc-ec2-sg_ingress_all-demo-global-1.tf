5446651ab8a848f669616e2db12813710e39e004
ppresto <ppresto@hashicorp.com>
2020-01-23 16:30:45 -0800

    less secure

--- tfe/pub-mods-vpc-ec2-sg_ingress_all-demo-global-1.tf
+++ tfe/pub-mods-vpc-ec2-sg_ingress_all-demo-global-1.tf
@@ -22,7 +22,7 @@ module "myapp_sg" {
     {
       rule        = "postgresql-tcp"
       #cidr_blocks = "10.10.0.0/16"
-      cidr_blocks = "157.131.174.226/32"
+      cidr_blocks = "0.0.0.0/0"
     },
   ]
 }
