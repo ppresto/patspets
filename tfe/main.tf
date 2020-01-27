5203288dd4a22bb64f1894ef272c27f8d157692c
ppresto <ppresto@hashicorp.com>
2020-01-27 14:54:50 -0800

    default

--- tfe/main.tf
+++ tfe/main.tf
@@ -12,19 +12,19 @@ data "terraform_remote_state" "patrick_tf_aws_standard_network" {
 // Modules
 module "ec2_instance" {
   source  = "app.terraform.io/Patrick/ec2_instance/aws"
+  // version - Use 2.0.6/2.0.7 to test policy: use-latest-module-version
   version = "2.0.8"
-
   name_prefix = "${var.name_prefix}"
-  security_group = "${data.terraform_remote_state.patrick_tf_aws_standard_network.outputs.security_group_web}"
-
   instance_count = 5
-  instance_type = "t2.nano"
-    tags = {
+  instance_type = "t2.large"
+  security_group = "${data.terraform_remote_state.patrick_tf_aws_standard_network.outputs.security_group_web}"
+  tags = {
     Environment = "dev"
     owner       = "uswest-se-ppresto"
     TTL         = 24
   }
 }
+
 
 //--------------------------------------------------------------------
 // OUTPUTS - For Useability
