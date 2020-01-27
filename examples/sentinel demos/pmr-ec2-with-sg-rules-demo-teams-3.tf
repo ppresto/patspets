b53ef133d5e1c1aa3cb08b3c03ab371359888774
ppresto <ppresto@hashicorp.com>
2020-01-27 02:41:10 -0800

    test

--- examples/sentinel demos/pmr-ec2-with-sg-rules-demo-teams-3.tf
+++ examples/sentinel demos/pmr-ec2-with-sg-rules-demo-teams-3.tf
@@ -1,3 +1,49 @@
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.17.0"
+  //source  = "app.terraform.io/presto-workshop-tfc-aws/vpc/aws"
+  //version = "2.21.0"
+
+  name = "presto-vpc"
+  cidr = "10.10.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+  public_subnets  = ["10.10.101.0/24", "10.10.102.0/24", "10.10.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Environment = "presto-dev"
+  }
+}
+
+module "myapp_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "myapp-service"
+  description = "Security group for myapp-service with my custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "myapp-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
+
 //--------------------------------------------------------------------
 // Workspace Data
 data "terraform_remote_state" "patrick_tf_aws_standard_network" {
