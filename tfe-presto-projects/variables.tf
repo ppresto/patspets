94e0948f45e9460fe4da0b9507f0a7587790d9af
ppresto <ppresto@hashicorp.com>
2020-03-27 14:57:17 -0700

    presto-projects support

---
+++ tfe-presto-projects/variables.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-west-2"
+}
+
+variable "name_prefix" {
+  description = "Enter your name or unique description here."
+}
+
+variable "instance_type" {
+  description = "instance size (default: t2.micro)"
+  type        = "string"
+  default     = "t2.micro"
+}
+
+variable "ingress_cidr_block" {
+  description = "WARNING: USING 0.0.0.0/0 IS INSECURE! (ex: <public.ipaddress>/32)"
+  type        = "string"
+  default     = "157.131.174.226/32"
+}
+
+variable "vpc_cidrs_public" {
+  description = "VPC CIDR blocks for public subnets, defaults to \"10.139.1.0/24\", \"10.139.2.0/24\", and \"10.139.3.0/24\"."
+  type        = "list"
+  default     = ["10.139.1.0/24", "10.139.2.0/24", "10.139.3.0/24",]
+}
