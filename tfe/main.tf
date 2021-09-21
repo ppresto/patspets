fb124d14eff73028bbc6aa02511744ac5552dd6a
ppresto <ppresto@hashicorp.com>
2021-09-21 15:47:00 -0700

    2

--- tfe/main.tf
+++ tfe/main.tf
@@ -12,7 +12,7 @@ data "terraform_remote_state" "patrick_tf_aws_standard_network" {
 // Modules
 module "ec2_instance" {
   source  = "app.terraform.io/Patrick/ec2_instance/aws"
-  version = "2.0.7" - Use to verify policy: use-latest-module-version
+  version = "2.0.7" //Use to verify policy: use-latest-module-version
   //version = "2.0.8"
   name_prefix = "${var.name_prefix}"
   instance_count = 3
