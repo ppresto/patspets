fe6be43a42d3e64e5ba4f9ba63f5565a1a7fc602
ppresto <ppresto@hashicorp.com>
2021-09-22 15:38:57 -0700

    nano

--- tfe/main.tf
+++ tfe/main.tf
@@ -15,12 +15,12 @@ data "terraform_remote_state" "vpc" {
 // Modules
 module "ec2_instance" {
   source = "app.terraform.io/Patrick/ec2_instance/aws"
-  //version = "2.0.9"    //verify policy: use-latest-module-version
-  version        = "2.0.10"
+  version = "2.0.9"    //verify policy: use-latest-module-version
+  //version        = "2.0.10"
   subnet_id      = data.terraform_remote_state.vpc.outputs.public_subnets[0]
   name_prefix    = var.prefix
   instance_count = 3
-  instance_type  = "t2.large"
+  instance_type  = "t2.nano"
   security_group = data.terraform_remote_state.vpc.outputs.security_group_web
   tags           = var.tags
   //tags = {}
